509ec3cabb9da987dd70aacca906db8536e985b3
GTSBrad <liquid.brad@gmail.com>
2025-08-02 09:43:50 -0400

    Added CircleShape class to handle circular shapes

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()  # Initialize all imported pygame modules
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Set the screen size
+    framerate = pygame.time.Clock()  # Create a clock object to control the frame rate
+    dt = 0  # Initialize delta time
     #print("Starting Asteroids!")
     while True:
         for event in pygame.event.get():
@@ -16,6 +18,8 @@ def main():
         fill_color = (0, 0, 0)  # Black background
         screen.fill(fill_color)  # Fill the screen with the fill color
         pygame.display.flip()  # Update the full display Surface to the screen
+        framerate.tick(60)
+        dt += framerate.get_time() / 1000.0  # Update delta time in seconds
 
       
 if __name__ == "__main__":
